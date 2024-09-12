d58ab7d5faed0557fcc653aed4d31a314d07b0c1
it24102651 <it24102651@my.sliit.lk>
2024-09-12 20:43:38 +0530

    Add files via upload

---
+++ Q2/IT24102651Lab6Q2A.java
@@ -0,0 +1,9 @@
+public class IT24102651Lab6Q2A {
+    public static void main(String[] args) {
+        int i = 1; 
+        while (i <= 10) { 
+            System.out.print(i + " "); 
+            i++;
+        }
+    }
+}
